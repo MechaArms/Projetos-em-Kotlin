dbabccaaa55f5af356a38d2c9c4ef41397f459ed
Romero <103903074+MechaArms@users.noreply.github.com>
2022-08-25 14:46:18 -0300

    Desafio com funções

---
+++ Rendimento de Tinta.kt
@@ -0,0 +1,23 @@
+//Desafio com funções
+
+/*
+criar um programa que calcula a quantidade de tinta necessaria para pintar uma parede.
+O usuario deve fornecer as informações: rendimento, altura e largura
+o programa deve mostrar a mensagem no final: 'Você necessita de x latas de tinta.'
+uma lata rende 5m²
+*/
+
+fun latas_tinta (alt: Double, larg: Double, rend: Double): String{
+    var resultado: Double = (alt + larg) / rend
+    return "Você necessita de $resultado latas de tinta."
+}
+
+fun main() {
+    println("Digite qual o rendimento da lata de tinta: ")
+    val alt = readln().toDouble()
+    println("Digite a Altura da parede: ")
+    val larg = readln().toDouble()
+    println("Digite a Largura da parede: ")
+    val rend = readln().toDouble()
+    latas_tinta (alt, larg, rend)
+}
