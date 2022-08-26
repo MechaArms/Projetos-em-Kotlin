32f6e897aa4ab90dcf4b0e476a5f59bfff2dbb15
Romero <103903074+MechaArms@users.noreply.github.com>
2022-08-26 08:45:10 -0300

    Desafio com funções

--- Rendimento de Tinta.kt
+++ Rendimento de Tinta.kt
@@ -1,11 +1,15 @@
-//Desafio com funções
-
 /*
-criar um programa que calcula a quantidade de tinta necessaria para pintar uma parede.
+Usando a versão do koltin: 1.7.10
+
+Desafio com funções
+
+Criar um programa que calcula a quantidade de tinta necessaria para pintar uma parede.
 O usuario deve fornecer as informações: rendimento, altura e largura
 o programa deve mostrar a mensagem no final: 'Você necessita de x latas de tinta.'
 uma lata rende 5m²
 */
+
+import java.util.Scanner
 
 fun latas_tinta (alt: Double, larg: Double, rend: Double): String{
     var resultado: Double = (alt + larg) / rend
@@ -15,11 +19,10 @@ fun latas_tinta (alt: Double, larg: Double, rend: Double): String{
 
 fun main() {
     println("Digite qual o rendimento da lata de tinta: ")
-    val alt = readln().toDouble()
-    println("Digite a Altura da parede: ")
-    val larg = readln().toDouble()
-    println("Digite a Largura da parede: ")
     val rend = readln().toDouble()
+    println("Digite a Altura da parede: ")
+    val alt = readln().toDouble()
+    println("Digite a Largura da parede: ")
+    val larg = readln().toDouble()
     println(latas_tinta(alt, larg, rend))
 }
-// koltin up 1.6v
