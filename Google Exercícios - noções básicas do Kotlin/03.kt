b262fda1c0ebd281368346ff25eff5eb3e821b85
Romero <103903074+MechaArms@users.noreply.github.com>
2022-09-09 08:48:14 -0300

    Create 03.kt

---
+++ Google Exercícios - noções básicas do Kotlin/03.kt
@@ -0,0 +1,25 @@
+/*
+3. Corrigir erros de compilação
+
+Este programa gera para o usuário um aviso de que ele recebeu uma mensagem de bate-papo de um amigo.
+
+
+	fun main() {
+		println("New chat message from a friend'}
+	}
+	
+	1. Você consegue descobrir e corrigir a causa raiz dos erros de compilação nesse programa?
+	2. O código usa os símbolos adequados para indicar o argumento de abertura e fechamento do argumento da string e da função?
+
+Dica: você pode usar o Playground Kotlin para executar o código e ver os erros de compilação.
+
+Após a correção dos erros, o programa vai ser compilado sem erros e gerar esta saída:
+
+
+	New chat message from a friend
+
+*/
+
+fun main() {
+    println("New chat message from a friend")
+}
